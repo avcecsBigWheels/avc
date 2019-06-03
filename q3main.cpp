af528f505a3f8e8cb43f4da49faf88cf100bdad5
avcecsBigWheels <51065580+avcecsBigWheels@users.noreply.github.com>
2019-06-04 10:41:41 +1200

    Update q3main.cpp
    maybe

--- q3main.cpp
+++ q3main.cpp
@@ -265,21 +265,21 @@ void Robot::maze() {
 		v_right = 49;
 		v_left = 56;
 		SetMotors();
-		sleep1 (115);
+		sleep1 (150);
 		printf ("Turn Left\n");
 	}
 	else if (junction) {
 		sleep1 (100);
 		v_right = 49;
 		v_left = 56;
-		sleep1 (115);
+		sleep1 (150);
 		printf ("Junction\n");
 	}
 	else if (deadEnd) {
 		v_right = 54;
 		v_left = 50;
 		SetMotors();
-		sleep1 (250);
+		sleep1 (400);
 		printf ("DeadEnd\n");
 	}
 	else if (goStraight) {
@@ -291,7 +291,7 @@ void Robot::maze() {
 		v_right = 49;
 		v_left = 56;
 		SetMotors();
-		sleep1 (115);
+		sleep1 (150);
 		printf ("Turn Left\n");
 	}
 	else if (turnRight) {
@@ -299,7 +299,7 @@ void Robot::maze() {
 		v_right = 39;
 		v_left = 48;
 		SetMotors();
-		sleep1 (115);
+		sleep1 (150);
 		printf ("Turn Right\n");
 	}	
 }
